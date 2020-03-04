e37e54fa63348a578763cabd48e559b0ba61266f
rajasekaran.s <proffraja@gmail.com>
2020-03-04 14:11:11 +0530

    working

--- src/main/resources/static/ops.js
+++ src/main/resources/static/ops.js
@@ -1,6 +1,39 @@
+/*
 function reloadAndView(){
     window.location.reload();
     document.getElementById("viewall").click();
+}
+function displayAsTable(result){
+    var i;
+    var userTable = document.getElementById("userdata");
+    var userTableBody = userTable.getElementsByTagName("tbody")[0];
+    for (i = 0; i < result.length; i++) {
+        var row = userTableBody.insertRow(i);
+        var userIdCell = row.insertCell(0);
+        var userNameCell = row.insertCell(1);
+        userIdCell.innerHTML = result[i].id;
+        userNameCell.innerHTML = result[i].name;
+    }
+} */
+
+function clearInputs(){
+    document.getElementById("id").innerHTML = '';
+    document.getElementById("name").innerHTML = '';
+}
+function displayAsTable(result){
+    var html = "";
+    var i;
+    html = html+"<table>";
+    html = html+"<tr>";
+    html = html+"<th>ID</th><th>Username</th>";
+    for(i=0;i<result.length;i++){
+        html = html+"<tr>";
+        html = html+"<td>"+result[i].id+"</td><td>"+result[i].name+"</td>";
+        html = html+"<tr>";
+    }
+    html = html+"</tr>";
+    html = html+"</table>";
+    document.getElementById("box").innerHTML = html;
 }
 function getAllUsers() {
     fetch('http://localhost:8080/alluser', {
@@ -10,22 +43,14 @@ function getAllUsers() {
         )
         .then((result) => {
             //content = content+"<tr><td>"+result.id+"</td><td>"+result.name+"</td></tr>";
-            var i;
-            var userTable = document.getElementById("userdata");
-            var userTableBody = userTable.getElementsByTagName("tbody")[0];
-            for (i = 0; i < result.length; i++) {
-                var row = userTableBody.insertRow(i);
-                var userIdCell = row.insertCell(0);
-                var userNameCell = row.insertCell(1);
-                userIdCell.innerHTML = result[i].id;
-                userNameCell.innerHTML = result[i].name;
-            }
+            displayAsTable(result);
+            clearInputs();
             console.log('Success:', result);
         })
         .catch((error) => {
             console.error('Error:', error);
         });
-
+        
 }
 
 function addUser() {
@@ -44,12 +69,13 @@ function addUser() {
         body: JSON.stringify(newuser)})
     
     .then(function(res){
+        getAllUsers();
         return res.json();
     })
     .catch(function(res){
         alert(JSON.stringify(newuser))
     });
-    reloadAndView();
+    
 }
 function updateUser() {
     var uid = document.getElementById("id").value;
@@ -67,6 +93,7 @@ function updateUser() {
         body: JSON.stringify(newuser)})
     
     .then(function(res){
+        getAllUsers();
         return res.json();
     })
     .catch(function(res){
@@ -78,5 +105,8 @@ function deleteUser() {
     var uid = document.getElementById("id").value;
     fetch('http://localhost:8080/delete/'+uid, {
         method: 'DELETE'
+        })
+        .then(function(){
+            getAllUsers();  
         });
 }

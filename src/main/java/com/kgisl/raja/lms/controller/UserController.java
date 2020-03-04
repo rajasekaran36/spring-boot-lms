71aeac0bce958666785e9b95f217bccdadbb20e2
rajasekaran.s <proffraja@gmail.com>
2020-03-04 11:57:33 +0530

    fully working

--- src/main/java/com/kgisl/raja/lms/controller/UserController.java
+++ src/main/java/com/kgisl/raja/lms/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.kgisl.raja.lms.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.kgisl.raja.lms.model.User;
 import com.kgisl.raja.lms.repository.UserRepository;
@@ -9,24 +10,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 @CrossOrigin(origins = "*")
 @RestController("/")
-public class UserController{
+public class UserController {
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/alluser")
-    public @ResponseBody ResponseEntity<List<User>> all(){
-        return new ResponseEntity<List<User>>(userRepository.findAll(),HttpStatus.OK);
+    public @ResponseBody ResponseEntity<List<User>> all() {
+        return new ResponseEntity<List<User>>(userRepository.findAll(), HttpStatus.OK);
     }
 
-    @PostMapping(value="/adduser", headers="Accept=application/json")
-    public @ResponseBody ResponseEntity<User> addUser(@RequestBody User aUser){
-        return new ResponseEntity<User>(userRepository.save(aUser),HttpStatus.CREATED);
+    @PostMapping(value = "/adduser", headers = "Accept=application/json")
+    public @ResponseBody ResponseEntity<User> addUser(@RequestBody User aUser) {
+        return new ResponseEntity<User>(userRepository.save(aUser), HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/put", headers = "Accept=application/json")
+    public @ResponseBody ResponseEntity<User> updateUser(@RequestBody User aUser){
+        User x = userRepository.findById(aUser.getId()).get();
+        x.setName("xxxxx");
+        return new ResponseEntity<User>(userRepository.save(aUser),HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public void deleteUser(@PathVariable(value = "id") Long id){
+        userRepository.deleteById(id);
+
     }
 }
--- src/main/resources/static/ops.js
+++ src/main/resources/static/ops.js
@@ -1,30 +1,78 @@
 
-fetch('http://localhost:8080/alluser', {
-    method: 'GET',
-})
-    .then((response) => response.json()
-    )
-    .then((result) => {
-        //content = content+"<tr><td>"+result.id+"</td><td>"+result.name+"</td></tr>";
-        var i;
-        var content = "<table>";
-        for (i = 0; i < result.length; i++) {
-            content = content + "<tr><td>" + result[i].id + "</td><td>" + result[i].name + "</td></tr>";
-        }
-        content = content + "<table>";
-        console.log('Success:', result);
+function getAllUsers() {
+    fetch('http://localhost:8080/alluser', {
+        method: 'GET',
     })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+        .then((response) => response.json()
+        )
+        .then((result) => {
+            //content = content+"<tr><td>"+result.id+"</td><td>"+result.name+"</td></tr>";
+            var i;
+            var userTable = document.getElementById("userdata");
+            var userTableBody = userTable.getElementsByTagName("tbody")[0];
+            for (i = 0; i < result.length; i++) {
+                var row = userTableBody.insertRow(i);
+                var userIdCell = row.insertCell(0);
+                var userNameCell = row.insertCell(1);
+                userIdCell.innerHTML = result[i].id;
+                userNameCell.innerHTML = result[i].name;
+            }
+            console.log('Success:', result);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 
-var uid = document.getElementById("id").value;
-var uname = document.getElementById("name").value;
-var addUser = fetch('http://localhost:8080/adduser', {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ id: uid, name: uname })
-});
+}
+
+function addUser() {
+    var uid = document.getElementById("id").value;
+    var uname = document.getElementById("name").value;
+    var newuser = {
+        id: uid,
+        name: uname
+    }
+    fetch('http://localhost:8080/adduser', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newuser)})
+    
+    .then(function(res){
+        return res.json();
+    })
+    .catch(function(res){
+        alert(JSON.stringify(newuser))
+    });
+}
+function updateUser() {
+    var uid = document.getElementById("id").value;
+    var uname = document.getElementById("name").value;
+    var newuser = {
+        id: uid,
+        name: uname
+    }
+    fetch('http://localhost:8080/put', {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newuser)})
+    
+    .then(function(res){
+        return res.json();
+    })
+    .catch(function(res){
+        alert(JSON.stringify(newuser))
+    });
+}
+
+function deleteUser() {
+    var uid = document.getElementById("id").value;
+    fetch('http://localhost:8080/delete/'+uid, {
+        method: 'DELETE'
+        });
+}
